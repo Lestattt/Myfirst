4db6235677df639b9380624c9e41b5739cc164ec
Aleksey <mikhaiilv09@ukr.net>
2019-01-29 21:29:48 +0200

    fdfd

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,12 +1,7 @@
 "use strict";
 
 function func() {
-	let elem = document.getElementById("test");
-	let elem1 = document.getElementById("test1");
-	elem.disabled = true;
-	elem1.innerHTML = "red";
-	elem1.style.color = "red";
-
+	
 	
 	
 
