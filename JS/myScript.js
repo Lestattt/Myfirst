0d1f0e1050d92d8cc980316218af0281d23a0f24
Aleksey <mikhaiilv09@ukr.net>
2019-02-05 21:35:44 +0200

    bla

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,8 +1,16 @@
 "use strict";
 
-function func() {
-	
-	
-	
+//Эта функция запускает таймер
+function start() {
+	window.timerId = window.setInterval(timer, 1000);
+}
+//Эта функция останавливает таймер
+function stop() {
+	window.clearInterval(window.timerId);
+}
 
-	}
+//Эта функция меняет value для инпута
+function timer() {
+	var elem = document.getElementById('test');
+	elem.value = parseInt(elem.value)+1;
+}
