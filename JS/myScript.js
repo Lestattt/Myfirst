aecda4545fda3fd65d4a79650f9c93abeb9c0682
Aleksey <mikhaiilv09@ukr.net>
2019-04-21 12:36:59 +0300

    efef

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,2 +1,9 @@
 "use strict";
+var div = document.getElementById('elem');
 
+div.addEventListener('click', func);
+
+function func(event) {
+	var target = event.target;
+	alert(target); //выведет 'p' - абзац
+}
